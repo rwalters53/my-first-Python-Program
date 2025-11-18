4d3910362cb143e10f39aefdf7181e0988844827
rwalters53 <rwalters53@ivytech.edu>
2025-11-18 04:17:12 -0500

    Add files via upload

---
+++ Pass:fail assignment.py
@@ -0,0 +1,94 @@
+Python 3.14.0 (v3.14.0:ebf955df7a8, Oct  7 2025, 08:20:14) [Clang 16.0.0 (clang-1600.0.26.6)] on darwin
+Enter "help" below or click "Help" above for more information.
+
+=============== RESTART: /Users/ryanwesleywalters/Documents/ds.py ==============
+Please enter your name:David
+
+=============== RESTART: /Users/ryanwesleywalters/Documents/ds.py ==============
+Please enter your name:David
+Please enter your grade:96
+
+=============== RESTART: /Users/ryanwesleywalters/Documents/ds.py ==============
+Please enter your name:David'
+Please enter your grade:92
+pass if number <60, else fail
+
+=============== RESTART: /Users/ryanwesleywalters/Documents/ds.py ==============
+Please enter your name:dVID
+Please enter your grade:95
+pass if number <60, else fail
+=============== RESTART: /Users/ryanwesleywalters/Documents/ds.py ==============
+Please enter your name:david
+Please enter your grade:92
+Hello + name, you passed this class
+
+=============== RESTART: /Users/ryanwesleywalters/Documents/ds.py ==============
+Please enter your name:David
+Enter your grade92
+Traceback (most recent call last):
+  File "/Users/ryanwesleywalters/Documents/ds.py", line 3, in <module>
+    if grade > 60:
+TypeError: '>' not supported between instances of 'str' and 'int'
+
+=============== RESTART: /Users/ryanwesleywalters/Documents/ds.py ==============
+Please enter your name:David
+Enter your grade:92
+Traceback (most recent call last):
+  File "/Users/ryanwesleywalters/Documents/ds.py", line 4, in <module>
+    if grade > 60:
+TypeError: '>' not supported between instances of 'str' and 'int'
+
+=============== RESTART: /Users/ryanwesleywalters/Documents/ds.py ==============
+Please enter your name:David
+Enter your grade: 82
+Hello David, you passed this class
+
+=============== RESTART: /Users/ryanwesleywalters/Documents/ds.py ==============
+Please enter your name:David
+Enter your grade: 92
+Traceback (most recent call last):
+  File "/Users/ryanwesleywalters/Documents/ds.py", line 4, in <module>
+    if grade > 60:
+TypeError: '>' not supported between instances of 'str' and 'int'
+>>> 
+=============== RESTART: /Users/ryanwesleywalters/Documents/ds.py ==============
+Please enter your name:David
+Traceback (most recent call last):
+  File "/Users/ryanwesleywalters/Documents/ds.py", line 2, in <module>
+    grade = num(input("Enter your grade: "))
+NameError: name 'num' is not defined. Did you mean: 'sum'?
+>>> 
+=============== RESTART: /Users/ryanwesleywalters/Documents/ds.py ==============
+Please enter your name:David
+Enter your grade: 92
+Hello David, you passed this class
+>>> 
+=============== RESTART: /Users/ryanwesleywalters/Documents/ds.py ==============
+Please enter your name:Kevin
+Enter your grade: 50
+Hello Kevin, you failed this class
+>>> 
+=============== RESTART: /Users/ryanwesleywalters/Documents/ds.py ==============
+Please enter your name:David
+Enter your grade: 92
+Hello David, you passed this class
+>>> 
+=============== RESTART: /Users/ryanwesleywalters/Documents/ds.py ==============
+Please enter your name:David
+Please enter your final grade: 95
+Hello David, you passed this class
+>>> 
+=============== RESTART: /Users/ryanwesleywalters/Documents/ds.py ==============
+Please enter your name:Kevin
+Please enter your final grade: 50
+Hello Kevin, you failed this class
+>>> 
+=============== RESTART: /Users/ryanwesleywalters/Documents/ds.py ==============
+Please enter your name:David
+Please enter your final grade: 95
+Hello David, you passed this class
+>>> 
+=============== RESTART: /Users/ryanwesleywalters/Documents/ds.py ==============
+Please enter your name:Kevin
+Please enter your final grade: 50
+Hello Kevin, you failed this class
