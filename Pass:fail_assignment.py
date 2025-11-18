d5e15dfcbff88fdea9184380485ae1704b43d0cf
rwalters53 <rwalters53@ivytech.edu>
2025-11-18 04:21:20 -0500

    Add files via upload

---
+++ Pass:fail_assignment.py
@@ -0,0 +1,7 @@
+name=input("Please enter your name:")
+grade = int(input("Please enter your final grade: "))
+
+if grade >= 60:
+    print("Hello " + name +", you passed this class")
+else:
+    print("Hello " + name + ", you failed this class")
